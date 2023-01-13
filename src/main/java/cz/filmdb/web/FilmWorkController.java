719386bdf0b74115879d30fb5a7407f01ac34733
Casgen <vzotov42@gmail.com>
2023-01-13 22:31:14 +0100

    Fixed the CORS problem, added a tweak to User object

--- src/main/java/cz/filmdb/web/FilmWorkController.java
+++ src/main/java/cz/filmdb/web/FilmWorkController.java
@@ -2,11 +2,13 @@ package cz.filmdb.web;
 
 import cz.filmdb.service.FilmWorkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/filmworks/")
+@CrossOrigin("http://localhost:5173")
 public class FilmWorkController {
 
     private FilmWorkService filmWorkService;
--- src/main/java/cz/filmdb/web/ReviewController.java
+++ src/main/java/cz/filmdb/web/ReviewController.java
@@ -3,15 +3,13 @@ package cz.filmdb.web;
 import cz.filmdb.model.Review;
 import cz.filmdb.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/reviews/")
+@CrossOrigin("http://localhost:5173")
 public class ReviewController {
 
     private ReviewService reviewService;
