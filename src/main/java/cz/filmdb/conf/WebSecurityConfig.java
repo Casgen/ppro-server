33face1ed63ba77a7bb2e890f2fb7f530afad267
Casgen <vzotov42@gmail.com>
2023-01-27 11:37:43 +0100

    Updated the WebSecurityConfig to properly authenticate the users.

--- src/main/java/cz/filmdb/conf/WebSecurityConfig.java
+++ src/main/java/cz/filmdb/conf/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package cz.filmdb.conf;
 
 import cz.filmdb.model.UserRole;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -13,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.concurrent.ExecutionException;
+
 @Configuration
 // This annotation ensures that Spring Security is initialized
 @EnableWebSecurity
@@ -22,42 +25,11 @@ public class WebSecurityConfig {
     private final JWTAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
-
+/*
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf()
-            .disable()
-                .authorizeHttpRequests()
-                //These URLs won't require authentication
-                .requestMatchers(HttpMethod.GET,
-                        "/api/v1/users/**",
-                        "/api/v1/users",
-                        "/api/v1/movies/**",
-                        "/api/v1/movies",
-                        "/api/v1/tvshows/**",
-                        "/api/v1/tvshows",
-                        "/api/v1/reviews/**",
-                        "/api/v1/reviews",
-                        "/api/v1/people/**",
-                        "/api/v1/people",
-                        "/api/v1/genres/**",
-                        "/api/v1/genres",
-                        "/api/v1/files/**",
-                        "/api/v1/occupations/**"
-                        )
-                .permitAll()
-            .and()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/api/v1/auth/**")
-                .permitAll()
-            .and()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        "/api/v1/account/**",
-                        "/api/v1/account"
-                )
-                .hasAnyRole(UserRole.USER.name(),UserRole.ADMIN.name())
             .and()
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST,
@@ -121,79 +93,69 @@ public class WebSecurityConfig {
         http.cors();
 
         return http.build();
-    }
-
-    /*
-    @Bean
-    @Order(1)
-    public SecurityFilterChain filterChainPublic(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                //These URLs won't require authentication
-                .requestMatchers(HttpMethod.GET,
-                        "/api/v1/users/**",
-                        "/api/v1/users",
-                        "/api/v1/movies/**",
-                        "/api/v1/movies",
-                        "/api/v1/tvshows/**",
-                        "/api/v1/tvshows",
-                        "/api/v1/reviews/**",
-                        "/api/v1/reviews",
-                        "/api/v1/people/**",
-                        "/api/v1/people",
-                        "/api/v1/genres/**",
-                        "/api/v1/genres",
-                        "/api/v1/files/**",
-                        "/api/v1/occupations/**"
-
-                )
-                .permitAll()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST,
-                        "/api/v1/auth/**",
-                        "/api/v1/files/**" // TODO: Change the permissions later on. this is just for testing purposes
-                )
-                .permitAll()
-
-                .cors();
-        return http.build();
     }*/
 
-    /*
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChainMovie(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
-                .authorizeHttpRequests()
-                //These URLs won't require authentication
-                .requestMatchers(
-                        "/api/v1/users/**",
-                        "/api/v1/users",
-                        "/api/v1/movies/**",
-                        "/api/v1/movies",
-                        "/api/v1/tvshows/**",
-                        "/api/v1/tvshows",
-                        "/api/v1/reviews/**",
-                        "/api/v1/reviews",
+                .securityMatcher("/api/v1/movies/**",
                         "/api/v1/people/**",
-                        "/api/v1/people",
                         "/api/v1/genres/**",
-                        "/api/v1/genres",
-                        "/api/v1/files/**",
-                        "/api/v1/occupations/**"
+                        "/api/v1/occupations/**",
+                        "/api/v1/users/**",
+                        "/api/v1/tvshows/**",
+                        "/api/v1/reviews/**",
+                        "/api/v1/files/**"
                 )
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .requestMatchers(HttpMethod.POST).hasAuthority(UserRole.ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE).hasAuthority(UserRole.ADMIN.name())
+                .requestMatchers(HttpMethod.PUT).hasAuthority(UserRole.ADMIN.name())
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        http.cors();
-
         return http.build();
-    }*/
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .securityMatcher("/api/v1/auth/**")
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST).permitAll();
+        return http.build();
+    }
+
+    @Bean
+    @Order(3)
+    public SecurityFilterChain filterChainPerson(HttpSecurity http) throws Exception {
+        // This option defines if the request can be sent from a different form than its origin.
+        // It enables us to use Postman without errors.
+        http.csrf()
+                .disable()
+                .securityMatcher("/api/v1/account/**")
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST).hasAnyAuthority(UserRole.USER.name(), UserRole.ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE).hasAnyAuthority(UserRole.USER.name(), UserRole.ADMIN.name())
+                .and()
+                //We have to provide the authentication for every separate filter chain!
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+
 }
